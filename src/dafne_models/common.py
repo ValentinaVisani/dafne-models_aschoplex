ff3fdc06d8cdfa66352e8cb7e1483db367664129
Francesco Santini <francesco.santini@gmail.com>
2023-06-26 15:55:53 +0100

    UNETR model from Kanishka Sharma

--- src/dafne_models/common.py
+++ src/dafne_models/common.py
@@ -10,7 +10,9 @@ def generate_convert(model_id,
                      model_name_prefix,
                      model_create_function,
                      model_apply_function,
-                     model_learn_function):
+                     model_learn_function,
+                     dimensionality=2,
+                     model_type=DynamicDLModel):
     """
     Function that either generates a new model using the default weights, or updates an existing model, based on argv[1]
 
@@ -21,6 +23,8 @@ def generate_convert(model_id,
         model_create_function: the function that creates the model
         model_apply_function: the function that applies the model
         model_learn_function: the function that performs the incremental learning of the model
+        dimensionality: the dimensionality of the data (2 or 3)
+        model_type: the type of the model (e.g. DynamicDLModel or DynamicTorchModel)
 
     Returns:
         None
@@ -30,7 +34,7 @@ def generate_convert(model_id,
         print("Converting model", sys.argv[1])
         old_model_path = sys.argv[1]
         filename = old_model_path
-        old_model = DynamicDLModel.Load(open(old_model_path, 'rb'))
+        old_model = model_type.Load(open(old_model_path, 'rb'))
         shutil.move(old_model_path, old_model_path + '.bak')
         weights = old_model.get_weights()
         timestamp = old_model.timestamp_id
@@ -39,16 +43,22 @@ def generate_convert(model_id,
         model_id = model_id
         timestamp = 1610001000
         model = model_create_function()
-        model.load_weights(default_weights_path)
+        try:
+            model.load_weights(default_weights_path)
+        except AttributeError:
+            # model is pytorch, not keras
+            import torch
+            model.load_state_dict(torch.load(default_weights_path))
         weights = model.get_weights()
         filename = f'models/{model_name_prefix}_{timestamp}.model'
 
-    modelObject = DynamicDLModel(model_id,
+    modelObject = model_type(model_id,
                                  model_create_function,
                                  model_apply_function,
                                  incremental_learn_function=model_learn_function,
                                  weights=weights,
-                                 timestamp_id=timestamp
+                                 timestamp_id=timestamp,
+                                 data_dimensionality=dimensionality
                                  )
 
     with open(filename, 'wb') as f:
